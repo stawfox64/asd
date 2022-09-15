caedad7bdf974b5c45e6e4eb9f23147ed05bbea9
stawfox64 <106298544+stawfox64@users.noreply.github.com>
2022-09-14 22:40:19 -0700

    Add files via upload

---
+++ datatype1.php
@@ -0,0 +1,23 @@
+<?php
+
+$First_Name= Marcelino 
+$Last_Name= Martinez
+$poem ="Poem by Sawan Dhyani"
+$animal = dog
+
+echo "my name is" . " ". $First_Name . " " . $Last_Name, <br/>;
+echo $poem,"<br/>";
+echo "my faorite animal is a dog <br/>"
+echo "<br/>";
+
+?>
+<?php
+$poem_line1="Time will heal the wounds,"
+$poem_line2="Everything will be green again"
+$poem_line3="Teh scars will soon fade away"
+$poem_line4="And Dry country will be blessed with rain"
+
+$complete_lines=$poem_line1
+$complete_lines.= $poem_line2
+$complete_lines.=$poem_line3
+$complete_lines.+$poem_line4
